6993d7cee4be193beb2f40f66166cfdb11ee9f25
SneiderVianaCastillo <sneiderviana7@gmail.com>
2020-06-19 09:01:19 -0500

    commit1

--- Covid/Form1.Designer.cs
+++ Covid/Form1.Designer.cs
@@ -28,13 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.EntidadCombo = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.CargarBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(163, 89);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "ENTIDAD";
+            // 
+            // EntidadCombo
+            // 
+            this.EntidadCombo.FormattingEnabled = true;
+            this.EntidadCombo.Location = new System.Drawing.Point(240, 86);
+            this.EntidadCombo.Name = "EntidadCombo";
+            this.EntidadCombo.Size = new System.Drawing.Size(224, 21);
+            this.EntidadCombo.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(110, 151);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(108, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "FECHA DE CARGUE";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(240, 145);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(224, 20);
+            this.dateTimePicker1.TabIndex = 3;
+            // 
+            // CargarBtn
+            // 
+            this.CargarBtn.Location = new System.Drawing.Point(508, 89);
+            this.CargarBtn.Name = "CargarBtn";
+            this.CargarBtn.Size = new System.Drawing.Size(113, 58);
+            this.CargarBtn.TabIndex = 4;
+            this.CargarBtn.Text = "CARGAR";
+            this.CargarBtn.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(766, 247);
+            this.Controls.Add(this.CargarBtn);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.EntidadCombo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox EntidadCombo;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button CargarBtn;
     }
 }
 
--- Entiry/Persona.cs
+++ Entiry/Persona.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace Entiry
 {
-    public class Class1
+    public class Persona
     {
     }
 }
